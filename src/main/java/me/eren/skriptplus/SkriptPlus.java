91f307ae13befabd8f6b4da708c437cc1907380a
erenkarakal <67760502+erenkarakal@users.noreply.github.com>
2023-06-13 14:14:16 +0300

    Update SkriptPlus.java

--- src/main/java/me/eren/skriptplus/SkriptPlus.java
+++ src/main/java/me/eren/skriptplus/SkriptPlus.java
@@ -5,19 +5,35 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Properties;
 
 public final class SkriptPlus extends JavaPlugin {
-    private static SkriptPlus instance;
+    private static SkriptPlus INSTANCE;
     private static Properties addonProperties;
+    public static final String PREFIX = "<white>[<gold>Skript<yellow>+<white>] ";
 
     @Override
     public void onEnable() {
-        instance = this;
+        INSTANCE = this;
         getLogger().info("Enabled SkriptPlus v" + getDescription().getVersion());
         Skript.registerAddon(this);
+
+        this.getCommand("skript").setExecutor(new SkriptCommand());
+
+        if (!getDataFolder().exists() && !getDataFolder().mkdir())
+            throw new RuntimeException("Data directory doesn't exist and can't be created.");
+
+        final File properties = new File(getDataFolder(), "addon.properties");
+        try {
+            if (!properties.exists() && !properties.createNewFile())
+                throw new RuntimeException("addon.properties doesn't exist but can't be created.");
+            Files.copy(getResource("addon.properties"), properties.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -26,17 +42,21 @@ public final class SkriptPlus extends JavaPlugin {
     }
 
     public static SkriptPlus getInstance() {
-        return instance;
+        return INSTANCE;
     }
 
-    public static Properties getAddonProperties() throws IOException {
+    public static Properties getAddonProperties() {
         if (addonProperties == null) {
-            Properties properties = new Properties();
-            File file = new File("./plugins/SkriptPlus/addon.properties");
-            FileInputStream stream = new FileInputStream(file);
-            properties.load(stream);
-            stream.close();
-            addonProperties = properties;
+            try {
+                Properties properties = new Properties();
+                File file = new File("./plugins/SkriptPlus/addon.properties");
+                FileInputStream stream = new FileInputStream(file);
+                properties.load(stream);
+                stream.close();
+                addonProperties = properties;
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load addon.properties file.", e);
+            }
         }
         return addonProperties;
     }
